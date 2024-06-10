0741a2b8c8e5ad34839e3e6c238d1568eee0327b
resources01 <hk.resources01@gmail.com>
2024-06-10 14:04:50 +0700

    demo

---
+++ demo/88saigonphobanhmi/js/about.js
@@ -0,0 +1,179 @@
+/* JS Document */
+
+/******************************
+
+[Table of Contents]
+
+1. Vars and Inits
+2. Set Header
+3. Init Menu
+4. Init Date Picker
+5. Init Time Picker
+6. Init Test Slider
+
+
+******************************/
+
+//button control
+var btnTop = $('#button-top');
+var btnHome = $('#button-home');
+var btnHotline = $('#button-hotline');
+
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btnTop.addClass('show');
+    btnHome.addClass('show');
+    btnHotline.addClass('show');
+  } else {
+    btnTop.removeClass('show');
+    btnHome.removeClass('show');
+    btnHotline.removeClass('show');
+  }
+});
+
+btnTop.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '1200');
+});
+
+//end button control
+
+$(document).ready(function()
+{
+	"use strict";
+
+	/* 
+
+	1. Vars and Inits
+
+	*/
+
+	var header = $('.header');
+	var hamburgerBar = $('.hamburger_bar');
+	var hamburger = $('.hamburger');
+
+	setHeader();
+
+	$(window).on('resize', function()
+	{
+		setHeader();
+
+		setTimeout(function()
+		{
+			$(window).trigger('resize.px.parallax');
+		}, 375);
+	});
+
+	$(document).on('scroll', function()
+	{
+		setHeader();
+	});
+
+	initDatePicker();
+	initTimePicker();
+	initMenu();
+	initTestSlider();
+
+	/* 
+
+	2. Set Header
+
+	*/
+
+	function setHeader()
+	{
+		if($(window).scrollTop() > 91)
+		{
+			header.addClass('scrolled');
+			hamburgerBar.addClass('scrolled');
+		}
+		else
+		{
+			header.removeClass('scrolled');
+			hamburgerBar.removeClass('scrolled');
+		}
+	}
+
+	/* 
+
+	3. Init Menu
+
+	*/
+
+	function initMenu()
+	{
+		if($('.menu').length)
+		{
+			var menu = $('.menu');
+			hamburger.on('click', function()
+			{
+				hamburger.toggleClass('active');
+				menu.toggleClass('active');
+			});
+		}
+	}
+
+	/* 
+
+	4. Init Date Picker
+
+	*/
+
+	function initDatePicker()
+	{
+		var dp = $('#datepicker');
+		var date = new Date();
+		var dateM = date.getMonth() + 1;
+		var dateD = date.getDate();
+		var dateY = date.getFullYear();
+		var dateFinal = dateM + '/' + dateD + '/' + dateY;
+		dp.val(dateFinal);
+		dp.datepicker();
+	}
+
+	/* 
+
+	5. Init Time Picker
+
+	*/
+
+	function initTimePicker()
+	{
+		$('.timepicker').timepicker(
+		{
+		    interval: 60,
+		    minTime: '10',
+		    maxTime: '6:00pm',
+		    defaultTime: '11',
+		    startTime: '10:00',
+		    dynamic:  true,
+		    dropdown: true,
+		    scrollbar: true
+		});
+	}
+
+	/* 
+
+	6. Init Test Slider
+
+	*/
+
+	function initTestSlider()
+	{
+		if($('.test_slider').length)
+		{
+			var testSlider = $('.test_slider');
+			testSlider.owlCarousel(
+			{
+				items:1,
+				loop:true,
+				autoplay:false,
+				dots:false,
+				nav:false,
+				smartSpeed:1200
+			});
+		}
+	}
+
+});
